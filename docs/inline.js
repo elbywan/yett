89c38b939724147aa02e2cb086ba5d5f2dbb6220
elbywan <elbywan@hotmail.com>
2018-06-07 16:39:33 +0200

    Updated demo (style still sucks - but less than before)

--- docs/inline.js
+++ docs/inline.js
@@ -1,5 +1,5 @@
 (function(){
     var div = document.createElement('div')
-    div.innerHTML = 'The inline script added via a &lt;script&gt; tag has been executed.'
+    div.innerHTML = '<a href="https://github.com/snipsco/yett/blob/master/docs/inline.js" target="_blank">The inline script added via a &lt;script&gt; tag has been executed.</a>'
     document.getElementById('content').appendChild(div)
 })()
