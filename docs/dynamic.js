499827dc546910b49484476f79c2192accbc68b7
elbywan <elbywan@hotmail.com>
2018-06-13 13:39:08 +0200

    Update demo style

--- docs/dynamic.js
+++ docs/dynamic.js
@@ -1,5 +1,5 @@
 (function(){
     var div = document.createElement('div')
-    div.innerHTML = '<a href="https://github.com/snipsco/yett/blob/master/docs/dynamic.js" target="_blank">The script contained inside a dynamically added script tag has been executed.</a>'
+    div.innerHTML = '<a href="https://github.com/snipsco/yett/blob/master/docs/dynamic.js" target="_blank">The dynamically added script tag has been executed.</a>'
     document.getElementById('content').appendChild(div)
 })()
--- docs/inline.js
+++ docs/inline.js
@@ -1,5 +1,5 @@
 (function(){
     var div = document.createElement('div')
-    div.innerHTML = '<a href="https://github.com/snipsco/yett/blob/master/docs/inline.js" target="_blank">The inline script added via a &lt;script&gt; tag has been executed.</a>'
+    div.innerHTML = '<a href="https://github.com/snipsco/yett/blob/master/docs/inline.js" target="_blank">The inlined &lt;script&gt; tag has been executed.</a>'
     document.getElementById('content').appendChild(div)
 })()
