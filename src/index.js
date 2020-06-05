d6b725d956d1db15c8dbc0eb9c659abcd1460793
Fredrik Johansson <jeanfredrik@gmail.com>
2020-06-05 20:57:42 +0200

    Move all side effects to exported init function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import './observer'
-import './monkey'
+import { patterns } from './variables'
+import { observe } from './observer'
+import { monkey } from './monkey'
+
 export { unblock } from './unblock'
+
+export function init({ blacklist, whitelist } = {}) {
+    patterns.blacklist = (patterns.blacklist || blacklist) && [
+        ...(patterns.blacklist || []),
+        ...(blacklist || [])
+    ]
+    patterns.whitelist = (patterns.whitelist || whitelist) && [
+        ...(patterns.whitelist || []),
+        ...(whitelist || [])
+    ]
+    observe()
+    monkey()
+}
--- src/monkey.js
+++ src/monkey.js
@@ -8,49 +8,51 @@ const originalDescriptors = {
     type: Object.getOwnPropertyDescriptor(HTMLScriptElement.prototype, 'type')
 }
 
-// Monkey patch the createElement method to prevent dynamic scripts from executing
-document.createElement = function(...args) {
-    // If this is not a script tag, bypass
-    if(args[0].toLowerCase() !== 'script')
-        return createElementBackup.bind(document)(...args)
+export function monkey() {
+    // Monkey patch the createElement method to prevent dynamic scripts from executing
+    document.createElement = function (...args) {
+        // If this is not a script tag, bypass
+        if (args[0].toLowerCase() !== 'script')
+            return createElementBackup.bind(document)(...args)
 
-    const scriptElt = createElementBackup.bind(document)(...args)
+        const scriptElt = createElementBackup.bind(document)(...args)
 
-    // Define getters / setters to ensure that the script type is properly set
-    try {
-        Object.defineProperties(scriptElt, {
-            'src': {
-                get() {
-                    return originalDescriptors.src.get.call(this)
-                },
-                set(value) {
-                    if(isOnBlacklist(value, scriptElt.type)) {
-                        originalDescriptors.type.set.call(this, TYPE_ATTRIBUTE)
+        // Define getters / setters to ensure that the script type is properly set
+        try {
+            Object.defineProperties(scriptElt, {
+                'src': {
+                    get() {
+                        return originalDescriptors.src.get.call(this)
+                    },
+                    set(value) {
+                        if (isOnBlacklist(value, scriptElt.type)) {
+                            originalDescriptors.type.set.call(this, TYPE_ATTRIBUTE)
+                        }
+                        originalDescriptors.src.set.call(this, value)
                     }
-                    originalDescriptors.src.set.call(this, value)
+                },
+                'type': {
+                    set(value) {
+                        const typeValue = isOnBlacklist(scriptElt.src, scriptElt.type) ? TYPE_ATTRIBUTE : value
+                        originalDescriptors.type.set.call(this, typeValue)
+                    }
                 }
-            },
-            'type': {
-                set(value) {
-                    const typeValue = isOnBlacklist(scriptElt.src, scriptElt.type) ? TYPE_ATTRIBUTE : value
-                    originalDescriptors.type.set.call(this, typeValue)
-                }
-            }
-        })
+            })
 
-        // Monkey patch the setAttribute function so that the setter is called instead
-        scriptElt.setAttribute = function(name, value) {
-            if(name === 'type' || name === 'src')
-                scriptElt[name] = value
-            else
-                HTMLScriptElement.prototype.setAttribute.call(scriptElt, name, value)
+            // Monkey patch the setAttribute function so that the setter is called instead
+            scriptElt.setAttribute = function (name, value) {
+                if (name === 'type' || name === 'src')
+                    scriptElt[name] = value
+                else
+                    HTMLScriptElement.prototype.setAttribute.call(scriptElt, name, value)
+            }
+        } catch (error) {
+            // eslint-disable-next-line
+            console.warn(
+                'Yett: unable to prevent script execution for script src ', scriptElt.src, '.\n',
+                'A likely cause would be because you are using a third-party browser extension that monkey patches the "document.createElement" function.'
+            )
         }
-    } catch (error) {
-        // eslint-disable-next-line
-        console.warn(
-            'Yett: unable to prevent script execution for script src ', scriptElt.src, '.\n',
-            'A likely cause would be because you are using a third-party browser extension that monkey patches the "document.createElement" function.'
-        )
+        return scriptElt
     }
-    return scriptElt
 }
